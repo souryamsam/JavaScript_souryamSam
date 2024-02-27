f8d851d0c855cd2a0929b2520b4e60f2dcd4ba1c
Sourayam Sam <108325634+souryamsam@users.noreply.github.com>
2024-02-27 08:40:49 +0000

    container config done

---
+++ basic_of_js/variable.js
@@ -0,0 +1,6 @@
+const AcountId=12597563214;
+let Name="Souryam Sam";
+let gamilId="souryamsam@.in"
+let passWord="1258@Aa";
+let addhearNo;
+console.table([AcountId, Name, gamilId, passWord, addhearNo]);
