00a03c762395aa394012d1957b19e845538fee58
Sourayam Sam <108325634+souryamsam@users.noreply.github.com>
2024-02-28 08:21:08 +0000

    stringMethod

---
+++ basic_of_js/stringMethod.js
@@ -0,0 +1,10 @@
+const newString= new String("A-sailor went to-chop, chop, chop");
+//charAt()
+console.log(newString.charAt(10));
+console.log(newString.indexOf('w'));
+const newValu=newString.slice(-4,-5);
+console.log(newValu);
+console.log(newString.trim());
+console.log(newString.replace('w','W'));
+console.log(newString.includes('B'));
+console.log(newString.split('-'));
